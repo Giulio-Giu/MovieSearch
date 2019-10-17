212365ead760ff3087a26bec6fbffeeaabb13597
THIAGO_VIGNOLI_PONCIANO <71700471@ALFAB38.BARROCA>
2019-10-17 19:34:00 -0300

    first

---
+++ app/src/main/java/br/com/trabalhoomdb/ui/activities/HomeActivity.kt
@@ -0,0 +1,13 @@
+package br.com.trabalhoomdb.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.trabalhoomdb.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
