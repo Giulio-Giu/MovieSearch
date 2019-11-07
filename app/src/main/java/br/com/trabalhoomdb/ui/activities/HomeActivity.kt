ed4a73e939d1448b6f0351e794b74bdb84f32656
Giulio Machado <g.98.machado@hotmail.com>
2019-11-07 14:12:13 -0300

    pequenas alterações

--- app/src/main/java/br/com/trabalhoomdb/ui/activities/HomeActivity.kt
+++ app/src/main/java/br/com/trabalhoomdb/ui/activities/HomeActivity.kt
@@ -2,6 +2,8 @@ package br.com.trabalhoomdb.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import br.com.trabalhoomdb.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun setFragment(f: Fragment) {
+        val fm = supportFragmentManager.beginTransaction()
     }
 }
