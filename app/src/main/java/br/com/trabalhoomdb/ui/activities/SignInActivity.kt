e3c29472f19578750fa6076173266f8304531fc3
Giulio Machado <giulio.machado@fourtime.com>
2019-11-08 15:00:35 -0300

    finalizados o efetuar login e o cadastro

--- app/src/main/java/br/com/trabalhoomdb/ui/activities/SignInActivity.kt
+++ app/src/main/java/br/com/trabalhoomdb/ui/activities/SignInActivity.kt
@@ -1,13 +1,73 @@
 package br.com.trabalhoomdb.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import br.com.trabalhoomdb.R
+import br.com.trabalhoomdb.models.account.Account
+import br.com.trabalhoomdb.services.account.RetrofitInitializerAccount
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        tv_signIn_wrong_credentials.visibility = TextView.INVISIBLE
+
+        signIn_btn_signIn.setOnClickListener {
+            auth()
+        }
+
+        signIn_btn_signUp.setOnClickListener {
+            doSignUp()
+        }
+    }
+
+    fun doSignUp(){
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun auth() {
+        val s = RetrofitInitializerAccount().serviceAccount()
+
+        val account = Account()
+        account.email = et_signIn_email.text.toString()
+        account.password = et_signIn_password.text.toString()
+
+        val call = s.auth(account)
+
+        call.enqueue(object : Callback<Account> {
+
+            override fun onResponse(call: Call<Account>?, response: Response<Account>?) {
+                response?.let {
+                    if (it.code() == 200) {
+                        tv_signIn_wrong_credentials.visibility = TextView.INVISIBLE
+                        Toast.makeText(this@SignInActivity, "Autenticado!", Toast.LENGTH_LONG).show()
+                        gotoHome()
+                    } else {
+                        tv_signIn_wrong_credentials.visibility = TextView.VISIBLE
+//                        Toast.makeText(this@SignInActivity, "Usuário ou senha inválidos!", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Account>?, t: Throwable?) {
+                Toast.makeText(this@SignInActivity, "Ops, deu erro!", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    fun gotoHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/br/com/trabalhoomdb/ui/activities/SignUpActivity.kt
+++ app/src/main/java/br/com/trabalhoomdb/ui/activities/SignUpActivity.kt
@@ -2,12 +2,65 @@ package br.com.trabalhoomdb.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import br.com.trabalhoomdb.R
+import br.com.trabalhoomdb.models.account.Account
+import br.com.trabalhoomdb.services.account.RetrofitInitializerAccount
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        tv_signUp_message_error.visibility = TextView.INVISIBLE
+
+        signUp_btn_signUp.setOnClickListener {
+            if (et_signUp_password.text.toString() == et_signUp_confirmPassword.text.toString()) {
+                doSignUp()
+            } else {
+                tv_signUp_message_error.visibility = TextView.VISIBLE
+                tv_signUp_message_error.text = getString(R.string.error_passwords)
+            }
+        }
+
+        signUp_btn_cancel.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun doSignUp() {
+        val s = RetrofitInitializerAccount().serviceAccount()
+
+        val account = Account()
+        account.name = et_signUp_name.text.toString()
+        account.email = et_signUp_email.text.toString()
+        account.password = et_signUp_password.text.toString()
+
+        val call = s.auth(account)
+
+        call.enqueue(object : Callback<Account> {
+
+            override fun onResponse(call: Call<Account>?, response: Response<Account>?) {
+                response?.let {
+                    if(it.code() == 200) {
+                        Toast.makeText(this@SignUpActivity, "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show()
+                        finish()
+                    } else {
+                        tv_signUp_message_error.visibility = TextView.VISIBLE
+                        tv_signUp_message_error.text = getString(R.string.error_data)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Account>?, t: Throwable?) {
+                Toast.makeText(this@SignUpActivity, "Ops, deu erro!", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
