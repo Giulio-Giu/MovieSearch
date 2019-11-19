6755c0ff17b55985c6a0962a9799e7a1a62dc231
Giulio Machado <g.98.machado@hotmail.com>
2019-11-19 18:53:24 -0300

    finalizando chamada e retorno da api

--- app/src/main/java/br/com/trabalhoomdb/ui/fragments/HomeFragment.kt
+++ app/src/main/java/br/com/trabalhoomdb/ui/fragments/HomeFragment.kt
@@ -1,5 +1,7 @@
 package br.com.trabalhoomdb.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +10,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
 
 import br.com.trabalhoomdb.R
 import br.com.trabalhoomdb.models.omdb.Film
 import br.com.trabalhoomdb.services.omdb.RetrofitInitializerOmdb
 import br.com.trabalhoomdb.ui.activities.HomeActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_home.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,6 +28,7 @@ import retrofit2.Response
 class HomeFragment : Fragment() {
 
     val apiKey = "2f5cfd66"
+    var imageUrl = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,6 +53,12 @@ class HomeFragment : Fragment() {
                 searchFilm(et_movieSearchHint.text.toString().trim())
             }
         }
+
+        iv_movie_poster.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(imageUrl))
+            intent.setDataAndType(Uri.parse(imageUrl), "image/png")
+            startActivity(intent)
+        }
     }
 
     fun searchFilm(search: String) {
@@ -61,11 +72,15 @@ class HomeFragment : Fragment() {
 
             override fun onResponse(call: Call<Film>?, response: Response<Film>?) {
                 response?.let {
-                    if (it.body().Response.equals("true")) {
-                        getItems(it.body())
+                    if (it.body().Response.equals("true", ignoreCase = true)) {
+                        constraint_home_result.visibility = ConstraintLayout.VISIBLE
+                        getItems(it.body(), contextActivity)
                     } else {
-                        Toast.makeText(contextActivity, resources.getString(R.string.home_message_error_searchResult), Toast.LENGTH_LONG).show()
-                        
+                        Toast.makeText(
+                            contextActivity,
+                            resources.getString(R.string.home_message_error_searchResult),
+                            Toast.LENGTH_LONG
+                        ).show()
                         constraint_home_result.visibility = ConstraintLayout.INVISIBLE
                         tv_home_messageError.text = resources.getString(R.string.home_message_error_searchResult)
                         tv_home_messageError.visibility = TextView.VISIBLE
@@ -74,34 +89,29 @@ class HomeFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<Film>?, t: Throwable?) {
-                Toast.makeText(contextActivity, "Get some error", Toast.LENGTH_LONG).show()
+                Toast.makeText(contextActivity, getString(R.string.home_message_failure_enqueue), Toast.LENGTH_LONG)
+                    .show()
             }
         })
     }
 
-    fun getItems(film: Film) {
+    fun getItems(film: Film, contextActivity: HomeActivity) {
 
+        /**itens comuns*/
         tv_movie_title.text = film.Title
         tv_movie_imdbRating.text = film.imdbRating
         tv_movie_imdbVotes.text = film.imdbVotes
-        tv_movie_type.text = film.Type
+        val type = film.Type
         tv_movie_runtime.text = film.Runtime
         tv_movie_released.text = film.Released
         tv_movie_year.text = film.Year
         tv_movie_country.text = film.Country
 
-        //fazer separado o que for só de serie e o q for só de filme
-        val type = film.Type
-        if (type.equals("series")){
-            val type2 = "S" + type.substring(1,type.length)
+        Glide.with(contextActivity)
+            .load(film.Poster)
+            .into(iv_movie_poster)
 
-            // ....
-        } else {
-            val type2 = "M" + type.substring(1,type.length)
-
-            // ....
-        }
-        //fazer o poster com glide
+        imageUrl = film.Poster
 
         tv_movie_plot.text = film.Plot
         tv_movie_genre.text = film.Genre
@@ -109,7 +119,25 @@ class HomeFragment : Fragment() {
         tv_movie_writers.text = film.Writer
         tv_movie_awards.text = film.Awards
 
-        //
-        //
+        /**fazer separado o que for só de serie e o q for só de filme */
+        if (type.equals("series", ignoreCase = true)) {
+            tv_movie_type.text = "S" + type.substring(1, type.length)
+
+            group_show_only_series.visibility = Group.VISIBLE
+            group_show_only_movie.visibility = Group.GONE
+            tv_movie_totalSeasons.text = film.totalSeasons
+
+        } else {
+            tv_movie_type.text = "M" + type.substring(1, type.length)
+
+            group_show_only_series.visibility = Group.GONE
+            group_show_only_movie.visibility = Group.VISIBLE
+
+            tv_movie_director.text = film.Director
+            tv_movie_boxOffice.text = film.BoxOffice
+            tv_movie_production.text = film.Production
+
+        }
+
     }
 }
